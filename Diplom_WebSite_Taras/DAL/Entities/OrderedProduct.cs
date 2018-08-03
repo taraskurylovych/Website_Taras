90c40a68017fee1a43e158acb7ba9123afc7556d
Tarik <Tarik@MARYNA>
2018-08-03 21:41:39 +0300

    Add cart and orders

--- Diplom_WebSite_Taras/DAL/Entities/OrderedProduct.cs
+++ Diplom_WebSite_Taras/DAL/Entities/OrderedProduct.cs
@@ -7,20 +7,19 @@ using System.Web;
 
 namespace Diplom_WebSite_Taras.DAL.Entities
 {
-    public class Cart
+    public class OrderedProduct
     {
         [Key]
-        public int Id { get; set; }
-
-        public string CartId { get; set; }
-        [ForeignKey("Product")]
-        [Required(ErrorMessage = "Введіть назву товару")]
+        [Column(Order = 0)]
         public int ProductId { get; set; }
-        public int Count { get; set; }
 
-        
-        public DateTime DateCreated { get; set; }
+        [Key]
+        [Column(Order = 1)]
+        public int CustomerOrderId { get; set; }
+
+        public int Quantity { get; set; }
 
         public virtual Product Product { get; set; }
+        public virtual CustomerOrder CustomerOrder { get; set; }
     }
 }
--- Diplom_WebSite_Taras/Models/IdentityModels.cs
+++ Diplom_WebSite_Taras/Models/IdentityModels.cs
@@ -29,8 +29,11 @@ namespace Diplom_WebSite_Taras.Models
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Producer> Producers { get; set; }
-        
 
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<CustomerOrder> CustomerOrders { get; set; }
+        public DbSet<OrderedProduct> Orderedproducts { get; set; }
+        public DbSet<Cart> Carts { get; set; }
 
         public static ApplicationDbContext Create()
         {
